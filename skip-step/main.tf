cfc94876d8ed505c097c80222ea512d554e62974
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-21 22:20:19 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -12,34 +12,34 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name #"terraform-rg"
-  location = var.location #"eastus"
-}
-
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "lgaishtestnew"
-  location            = var.location
-  resource_group_name = var.rg_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "acraishtestnew"
-  resource_group_name = var.rg_name
-  location            = var.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-}
+#resource "azurerm_resource_group" "rg" {
+#  name     = var.rg_name #"terraform-rg"
+#  location = var.location #"eastus"
+#}
+#
+#resource "azurerm_log_analytics_workspace" "example" {
+#  name                = "lgaishtestnew"
+#  location            = var.location
+#  resource_group_name = var.rg_name
+#  sku                 = "PerGB2018"
+#  retention_in_days   = 30
+#}
+#
+#resource "azurerm_container_registry" "acr" {
+#  name                = "acraishtestnew"
+#  resource_group_name = var.rg_name
+#  location            = var.location
+#  sku                 = "Premium"
+#  admin_enabled       = false
+#  georeplications {
+#    location                = "East US"
+#    zone_redundancy_enabled = true
+#    tags                    = {}
+#  }
+#  georeplications {
+#    location                = "North Europe"
+#    zone_redundancy_enabled = true
+#    tags                    = {}
+#  }
+#}
 
