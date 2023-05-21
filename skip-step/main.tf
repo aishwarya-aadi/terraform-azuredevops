c10be259a3b92393fc61bdc50644cac37db80aae
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-21 23:50:08 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,16 +1,3 @@
-#terraform {
-#  required_providers {
-#    azurerm = {
-#      source  = "hashicorp/azurerm"
-#      version = "=3.9.0"
-#    }
-#  }
-#}
-#
-## Configure the Microsoft Azure Provider
-#provider "azurerm" {
-#  features {}
-#}
 terraform {
   backend "azurerm" {
   }
