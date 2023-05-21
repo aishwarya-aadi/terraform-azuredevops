31784f806d57ffab9036a4460fe89294957f03ca
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-21 23:16:34 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -12,19 +12,18 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "rg" {
-#  name     = var.rg_name #"terraform-rg"
-#  location = var.location #"eastus"
-#}
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name #"terraform-rg"
+  location = var.location #"eastus"
+}
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "lgaishtestnew"
+  location            = var.location
+  resource_group_name = var.rg_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 
-#resource "azurerm_log_analytics_workspace" "example" {
-#  name                = "lgaishtestnew"
-#  location            = var.location
-#  resource_group_name = var.rg_name
-#  sku                 = "PerGB2018"
-#  retention_in_days   = 30
-#}
-#
 #resource "azurerm_container_registry" "acr" {
 #  name                = "acraishtestnew"
 #  resource_group_name = var.rg_name
