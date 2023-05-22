c5bf61ffe7e1d9a4a7306c92e3767836e01c9c78
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-22 10:41:40 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -39,3 +39,85 @@ resource "azurerm_resource_group" "rg" {
 #  }
 #}
 
+#resource "azurerm_kubernetes_cluster" "example" {
+#  name                = "example-aks1"
+#  location            = azurerm_resource_group.example.location
+#  resource_group_name = azurerm_resource_group.example.name
+#  dns_prefix          = "exampleaks1"
+#
+#  default_node_pool {
+#    name       = "default"
+#    node_count = 1
+#    vm_size    = "Standard_D2_v2"
+#  }
+#
+#  identity {
+#    type = "SystemAssigned"
+#  }
+#
+#  tags = {
+#    Environment = "Production"
+#  }
+#}
+#
+#output "client_certificate" {
+#  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+#  sensitive = true
+#}
+#
+#output "kube_config" {
+#  value = azurerm_kubernetes_cluster.example.kube_config_raw
+#
+#  sensitive = true
+#}
+
+#resource "azurerm_key_vault" "example" {
+#  name                        = "examplekeyvault"
+#  location                    = azurerm_resource_group.example.location
+#  resource_group_name         = azurerm_resource_group.example.name
+#  enabled_for_disk_encryption = true
+#  tenant_id                   = data.azurerm_client_config.current.tenant_id
+#  soft_delete_retention_days  = 7
+#  purge_protection_enabled    = false
+#
+#  sku_name = "standard"
+#
+#  access_policy {
+#    tenant_id = data.azurerm_client_config.current.tenant_id
+#    object_id = data.azurerm_client_config.current.object_id
+#
+#    key_permissions = [
+#      "Get",
+#    ]
+#
+#    secret_permissions = [
+#      "Get",
+#    ]
+#
+#    storage_permissions = [
+#      "Get",
+#    ]
+#  }
+#}
+#resource "azurerm_virtual_network" "example" {
+#  name                = "example-vnet"
+#  address_space       = ["10.0.0.0/16"]
+#  location            = azurerm_resource_group.example.location
+#  resource_group_name = azurerm_resource_group.example.name
+#}
+#
+#resource "azurerm_subnet" "example" {
+#  name                 = "example-subnet"
+#  resource_group_name  = azurerm_resource_group.example.name
+#  virtual_network_name = azurerm_virtual_network.example.name
+#  address_prefixes     = ["10.0.1.0/24"]
+#
+#  delegation {
+#    name = "delegation"
+#
+#    service_delegation {
+#      name    = "Microsoft.ContainerInstance/containerGroups"
+#      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+#    }
+#  }
+#}
