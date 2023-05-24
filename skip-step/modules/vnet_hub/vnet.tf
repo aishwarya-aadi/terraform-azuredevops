5fbd28b48e1a973d467e28860d9b8ae879f61144
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 19:51:19 +0530

    Update vnet.tf

--- skip-step/modules/vnet_hub/vnet.tf
+++ skip-step/modules/vnet_hub/vnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.VnetHubName #"vnet-${var.Env}-${var.Location}"
   address_space       = var.VnetHubIp
   location            = var.Location
-  resource_group_name = var.RgHubName#"rg-network-${var.Env}-${var.Location}"
+  resource_group_name = var.RgHubName #"rg-network-${var.Env}-${var.Location}"
  #  depends_on = [
  #   azurerm_resource_group.rg
  #] 
