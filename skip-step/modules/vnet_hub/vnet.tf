a25e45616f5ac48769c5da8a8c9b67442666bd6d
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 18:09:19 +0530

    Update vnet.tf

--- skip-step/modules/vnet_hub/vnet.tf
+++ skip-step/modules/vnet_hub/vnet.tf
@@ -5,5 +5,5 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = var.RgHubName#"rg-network-${var.Env}-${var.Location}"
  #  depends_on = [
  #   azurerm_resource_group.rg
- #]
+ #] 
 }
