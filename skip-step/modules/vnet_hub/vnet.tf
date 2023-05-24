b31bdf7f0ef3d13f226fe979b65a53567f703ff1
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 17:54:02 +0530

    Create vnet.tf

---
+++ skip-step/modules/vnet_hub/vnet.tf
@@ -0,0 +1,9 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.VnetHubName #"vnet-${var.Env}-${var.Location}"
+  address_space       = var.VnetHubIp
+  location            = var.Location
+  resource_group_name = var.RgHubName#"rg-network-${var.Env}-${var.Location}"
+   depends_on = [
+    azurerm_resource_group.rg
+ ]
+}
