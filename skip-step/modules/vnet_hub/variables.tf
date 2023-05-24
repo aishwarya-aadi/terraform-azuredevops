d08b5da1baa7483ecf55e1479b49668f58015aae
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 17:54:31 +0530

    Create variables.tf

---
+++ skip-step/modules/vnet_hub/variables.tf
@@ -0,0 +1,26 @@
+variable "Env" {
+    type = string
+    
+}
+
+variable "Location" {
+    type = string
+   
+}
+
+
+variable "VnetHubIp" {
+  type = any
+  default = []
+}
+
+variable "RgHubName" {
+  type = any
+  default = []
+}
+
+
+variable "VnetHubName" {
+  type = any
+  default = []
+}
