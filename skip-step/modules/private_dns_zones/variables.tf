498ed882cffd68150b31e907f7336bb93ff84b3f
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 20:57:41 +0530

    Create variables.tf

---
+++ skip-step/modules/private_dns_zones/variables.tf
@@ -0,0 +1,22 @@
+variable "DnsZone" {
+    type = string
+   
+}
+
+variable "RgHubName" {
+  type = string
+}
+
+variable "DnsZone" {
+    type = map(any)
+    default = {
+      DnsZone_1 = {
+        name  = "mydomain1.com"
+      }
+
+      DnsZone_2 = {
+        name = "mydomain2.com"
+      } 
+
+    }
+}    
