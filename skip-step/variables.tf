d94fc3b4cb8c4c7d4702b87ed1e509dbc8429b27
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 20:24:52 +0530

    Update variables.tf

--- skip-step/variables.tf
+++ skip-step/variables.tf
@@ -27,7 +27,31 @@ variable "RgHubName" {
 
 variable "subnets" {
     type = map(any)
-    default = {}
+    default = {
+      subnet_1 = {
+        name = "SubnetErGateway"
+        address_prefixes = ["10.0.1.0/24"]
+      }
+
+      subnet_2 = {
+        name = "SubnetFirewall"
+        address_prefixes =["10.0.0.0/24"]
+      } 
+
+      subnet_3 = {
+        name = "SubnetDnsPrInbound"
+        address_prefixes =["10.0.2.0/24"]        
+      }
+
+      subnet_4 = {
+        name = "SubnetDnsPrOutbound"
+        address_prefixes =["10.0.3.0/24"]        
+      }
+      subnet_5 = {
+        name = "SubnetPrivateLink"
+        address_prefixes =["10.0.4.0/24"]        
+      }
+    } 
 }
 #variable "Env" {
 #    type = string
