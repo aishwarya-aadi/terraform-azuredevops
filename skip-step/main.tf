55afad4c27ef21bd04a004e8ffa6bb6202c975a5
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 17:36:57 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,26 +1,26 @@
-#terraform {
-#  backend "azurerm" {
-#  }
-#}
-#
-#provider "azurerm" {
-#  version = ">=2.0"
-#  # The "feature" block is required for AzureRM provider 2.x.
-#  features {}
-#}
-#
-#resource "azurerm_resource_group" "rg" {
-#  name     = "rg-network-${var.Env}-${var.Location}"
-#  location = "${var.Location}"
-#}
-#
-#resource "azurerm_virtual_network" "vnet" {
-#  name                = "vnet-${var.Env}-${var.Location}"
-#  address_space       = var.VnetHubIp
-#  location            = var.Location
-#  resource_group_name = "rg-network-${var.Env}-${var.Location}"
-#}
-#
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-network-${var.Env}-${var.Location}"
+  location = "${var.Location}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.Env}-${var.Location}"
+  address_space       = var.VnetHubIp
+  location            = var.Location
+  resource_group_name = "rg-network-${var.Env}-${var.Location}"
+}
+
 #resource "azurerm_subnet" "subnet" {
 #    for_each = var.subnets
 #    resource_group_name = "rg-network-${var.Env}-${var.Location}"
