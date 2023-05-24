bfad9c324461ea74290f47cd6d23cb8bd153c5fb
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 16:03:21 +0530

    Update variables.tf

--- skip-step/variables.tf
+++ skip-step/variables.tf
@@ -22,32 +22,32 @@ variable "VnetHubIp" {
   default = ["10.0.0.0/16"]
 }
 
-#variable "subnets" {
-#    type = map(any)
-#    default = {
-#      subnet_1 = {
-#        name = "SubnetErGateway"
-#        address_prefixes = ["10.0.1.0/24"]
-#      }
-#
-#      subnet_2 = {
-#        name = "SubnetFirewall"
-#        address_prefixes =["10.0.1.0/25"]
-#      } 
-#
-#      subnet_3 = {
-#        name = "SubnetDnsPrInbound"
-#        address_prefixes =["10.0.1.0/26"]        
-#      }
-#
-#      subnet_4 = {
-#        name = "SubnetDnsPrOutbound"
-#        address_prefixes =["10.0.1.0/27"]        
-#      }
-#      subnet_5 = {
-#        name = "SubnetPrivateLink"
-#        address_prefixes =["10.0.1.0/28"]        
-#      }
-#    } 
-#}
+variable "subnets" {
+    type = map(any)
+    default = {
+      subnet_1 = {
+        name = "SubnetErGateway"
+        address_prefixes = ["10.0.1.0/24"]
+      }
+
+      subnet_2 = {
+        name = "SubnetFirewall"
+        address_prefixes =["10.0.1.0/25"]
+      } 
+
+      subnet_3 = {
+        name = "SubnetDnsPrInbound"
+        address_prefixes =["10.0.1.0/26"]        
+      }
+
+      subnet_4 = {
+        name = "SubnetDnsPrOutbound"
+        address_prefixes =["10.0.1.0/27"]        
+      }
+      subnet_5 = {
+        name = "SubnetPrivateLink"
+        address_prefixes =["10.0.1.0/28"]        
+      }
+    } 
+}
 
