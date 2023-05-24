1c6777520608dc7c89246143a970229ed0ed415e
aishwarya-aadi <62787406+aishwarya-aadi@users.noreply.github.com>
2023-05-24 17:50:21 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -9,6 +9,32 @@ provider "azurerm" {
   features {}
 }
 
+module "resource_group"{
+  source = "./modules/resource_group_hub"
+  name     = var.RgHubName
+  location = var.Location
+}
+
+
+module "vnet"{
+  source = "./modules/vnet_hub"
+  name                = var.VnetHubName #"vnet-${var.Env}-${var.Location}"
+  address_space       = var.VnetHubIp
+  location            = var.Location
+  resource_group_name = var.RgHubName
+}
+
+
+module "subnet" {
+  for_each = var.subnets
+  source = "./modules/vnet_subnet_hub"
+  resource_group_name = var.RgHubName #"rg-network-${var.Env}-${var.Location}"
+  virtual_network_name = var.VnetHubName
+  name = each.value["name"]
+  address_prefixes = each.value["address_prefixes"]
+}
+
+
 #resource "azurerm_resource_group" "rg" {
 #  name     = "rg-network-${var.Env}-${var.Location}"
 #  location = "${var.Location}"
